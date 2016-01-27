aea64ce6803f2ae30f2dd170d37877233b6799f3
Marten Klitzke <me@mortik.xyz>
2016-01-27 09:01:04 +0100

    only allow https urls

--- popup.js
+++ popup.js
@@ -7,7 +7,7 @@ function closeAndReload() {
 }
 
 function isValidURL(url) {
-  var RegExp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  var RegExp = /https:\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 
   if (RegExp.test(url)) {
     return true;
